b59d29c462eea3e78a2107b186c42d404059997f
Baris Yavuz <55483569+brsyvz@users.noreply.github.com>
2021-11-04 20:24:35 +0300

    added async/await approach

--- script.js
+++ script.js
@@ -31,3 +31,37 @@ btn.addEventListener('click', function (e) {
       spinner.setAttribute('hidden', '');
     });
 });
+
+
+
+/* alternative async/await approach.
+async/await are just promises written in a different way. */
+
+// const spinner = document.getElementById('spinner');
+// let btn = document.querySelector('#btn');
+// const img = document.querySelector('img');
+
+// btn.addEventListener('click', randomGifs);
+// async function randomGifs(e) {
+//   try {
+//     e.preventDefault();
+//     spinner.removeAttribute('hidden');
+//     let text = document.getElementById('search');
+//     btn.textContent = `show me another ${text.value}`;
+//     img.src = '';
+//     const response = await fetch(
+//       `https://api.giphy.com/v1/gifs/translate?api_key=bcvSrcsUJTovc8VlAWL1TtqH8N7Bpal0&s=${text.value}`,
+//       {
+//         mode: 'cors',
+//       }
+//     );
+//     const gifData = await response.json();
+//     spinner.setAttribute('hidden', '');
+//     img.src = gifData.data.images.original.url;
+//     console.log('success');
+//   } catch (err) {
+//     console.log('fail');
+//     img.src = './error404.png';
+//     spinner.setAttribute('hidden', '');
+//   }
+// }
